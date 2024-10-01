9aa684fce6a9575184ff2b9083158ba457d6649b
Bhawesh Chaudhary <thisisbhawesh@gmail.com>
2024-10-01 09:55:50 +0545

    Update clijournal to version 1.0

--- Formula/clijournal.rb
+++ Formula/clijournal.rb
@@ -2,7 +2,7 @@ class Clijournal < Formula
     desc "advance CLI journal for productivity & note taking from terminal"
     homepage "https://github.com/bhaweshchaudhary/cli-journal/"
     url "https://github.com/bhaweshchaudhary/cli-journal/releases/download/CLI_Journal_V1.0/cli_journal-1.0-apple-darwin.tar.gz"
-    sha256 "4395b8f58e4709b9755dda0ebe59b5aa6155c191"
+    sha256 "888c298b6918666f7c253605ecdb6e07182656b7d0df957a05365c96881672a2"
     version "1.0"
   
     def install

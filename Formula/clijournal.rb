d6815eb2805719819835e8d209024acf9faab036
Bhawesh Chaudhary <thisisbhawesh@gmail.com>
2024-10-01 10:05:16 +0545

    Update clijournal description and fix newline issue

--- Formula/clijournal.rb
+++ Formula/clijournal.rb
@@ -1,5 +1,5 @@
 class Clijournal < Formula
-    desc "advance CLI journal for productivity & note taking from terminal"
+    desc "Advance CLI journal for productivity & note taking from terminal"
     homepage "https://github.com/bhaweshchaudhary/cli-journal/"
     url "https://github.com/bhaweshchaudhary/cli-journal/releases/download/CLI_Journal_V1.0/cli_journal-1.0-apple-darwin.tar.gz"
     sha256 "888c298b6918666f7c253605ecdb6e07182656b7d0df957a05365c96881672a2"
@@ -9,3 +9,4 @@ class Clijournal < Formula
       bin.install "clijournal"
     end
   end
+  
